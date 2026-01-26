e080c142271dc9a18f29053e5a4e074c3d39e6e3
Little-Mage <lm4ssw@gmail.com>
2026-01-25 20:52:26 -0700

    cb

--- premium.js
+++ premium.js
@@ -12,7 +12,9 @@ function premiumCalc() {
     const melt = silver * troy;
     const premium = (coin - melt).toFixed(2);
     const percent = (((coin - melt) / melt) * 100).toFixed(1);
+    let CB = (coin / troy).toFixed(2);
 
     document.getElementById('p1').innerHTML = `Premium: $${premium}`;
     document.getElementById('p2').innerHTML = `Percent: ${percent}%`;
+    document.getElementById('cb').innerHTML = `Cost Basis: $${CB}`;
 }
